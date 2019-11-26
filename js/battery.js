1ee08e41a80c5365f08bc6b839b76f681c8a2c8a
1 <1>
2019-11-26 15:00:26 +0800

    4

--- js/battery.js
+++ js/battery.js
@@ -4,8 +4,8 @@ window.onload = function () {
 }
 //测试开始
 function testBegin(val) {
-    var bcontent = document.getElementById("batteryIconProgress");
-    var label = document.getElementById("batteryIconContent");
+    var bcontent = document.getElementById("batteryIconProgress1");
+    var label = document.getElementById("batteryIconContent1");
     for (var i = val; i <= 100; i++) {
         bcontent.style.setProperty('width',  i+'%');
         if (i <= 20) {
@@ -21,4 +21,50 @@ function testBegin(val) {
         window.setTimeout("testBegin(" + ++i + ")", 200)
        break;
       }
+
+
+    var bcontent2 = document.getElementById("batteryIconProgress2");
+    var label2 = document.getElementById("batteryIconContent2");
+
+     var i2=parseFloat(label2.innerText);
+        bcontent2.style.setProperty('width',  i2+'%');
+        color2=batteryColor(i2);
+        bcontent2.style.setProperty('background', color2);
+       label2.innerText = i2+'%';
+
+    var bcontent3 = document.getElementById("batteryIconProgress3");
+    var label3 = document.getElementById("batteryIconContent3");
+
+     var i3=parseFloat(label3.innerText);
+        bcontent3.style.setProperty('width',  i3+'%');
+        color3=batteryColor(i3);
+        bcontent3.style.setProperty('background', color3);
+        
+       label3.innerText = i3+'%';
+
+    var bcontent4 = document.getElementById("batteryIconProgress4");
+    var label4 = document.getElementById("batteryIconContent4");
+
+     var i4=parseFloat(label4.innerText);
+        bcontent4.style.setProperty('width',  i4+'%');
+        color4=batteryColor(i4);
+        bcontent4.style.setProperty('background', color4);
+        
+       label4.innerText = i4+'%';
 }
+
+
+function batteryColor(x){
+    var color='';
+             if (x<= 30) {
+                  color='#ff0000';
+          }
+             if (x > 30) {
+                  color='#ffd800';
+          }
+             if (x > 60) {
+                  color='#00ff21';
+          }
+    return color
+}
+    
