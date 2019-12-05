13727e2dd9628329286eb1fa20c03807fcbe2f16
1 <1>
2019-12-05 17:04:30 +0800

    12.5

--- js/battery.js
+++ js/battery.js
@@ -1,18 +1,34 @@
+mydata = {
+	"name":"bms",
+	"battery": {
+        "1":{"u":"30",
+             "i":"-5",
+             "soc":"97"   
+            },
+        "2":{"u":"20",
+            "i":"-4",
+            "soc":"56"   
+           },
+        "3":{"u":"10",
+           "i":"-3",
+           "soc":"25"   
+          },
+	}
+}
+
+
+
 //窗体加载
 window.onload = function () {
     testBegin(0);
 }
 //测试开始
 function testBegin(val) {
-    var bcontent1 = document.getElementById("batteryIconProgress1");
-    var label1 = document.getElementById("batteryIconContent1");
+
     var bat7=document.getElementById("battery7");
 
     for (var i = val; i <= 100; i++) {
-        bcontent1.style.setProperty('width',  i+'%');
-        color1=batteryColor(i);
-        bcontent1.style.setProperty('background', color1);
-        label1.innerText = i+'%';
+       
 
         cname7=Findcname(i);
         bat7.style.setProperty('width',i+'%');
@@ -25,39 +41,35 @@ function testBegin(val) {
       }
 
 
-    var bcontent2 = document.getElementById("batteryIconProgress2");
-    var label2 = document.getElementById("batteryIconContent2");
+    var bat1=document.getElementById("battery1");
+    var soc1=mydata.battery[1]["soc"];
+    var u1=mydata.battery[1]["u"];
+    var i1=mydata.battery[1]["i"];
+    bat1.style.setProperty('width',soc1+'%');
+    bat1.className=Findcname(soc1);
+    bat1.innerText=soc1+'%';
+    $('#u1').text(u1);
+    $('#i1').text(i1);
+    
+    var bat2=document.getElementById("battery2");
+    var soc2=mydata.battery[2]["soc"];
+    var u2=mydata.battery[2]["u"];
+    var i2=mydata.battery[2]["i"];
+    bat2.style.setProperty('width',soc2+'%');
+    bat2.className=Findcname(soc2);
+    bat2.innerText=soc2+'%';
+    $('#u2').text(u2);
+    $('#i2').text(i2);
 
-     var i2=parseFloat(label2.innerText);
-        bcontent2.style.setProperty('width',  i2+'%');
-        color2=batteryColor(i2);
-        bcontent2.style.setProperty('background', color2);
-       label2.innerText = i2+'%';
-
-    var bcontent3 = document.getElementById("batteryIconProgress3");
-    var label3 = document.getElementById("batteryIconContent3");
-
-     var i3=parseFloat(label3.innerText);
-        bcontent3.style.setProperty('width',  i3+'%');
-        color3=batteryColor(i3);
-        bcontent3.style.setProperty('background', color3);
-        
-       label3.innerText = i3+'%';
-
-    var bcontent4 = document.getElementById("batteryIconProgress4");
-    var label4 = document.getElementById("batteryIconContent4");
-
-     var i4=parseFloat(label4.innerText);
-        bcontent4.style.setProperty('width',  i4+'%');
-        color4=batteryColor(i4);
-        bcontent4.style.setProperty('background', color4);
-        
-       label4.innerText = i4+'%';
-
-
-
-
-
+    var bat3=document.getElementById("battery3");
+    var soc3=mydata.battery[3]["soc"];
+    var u3=mydata.battery[3]["u"];
+    var i3=mydata.battery[3]["i"];
+    bat3.style.setProperty('width',soc3+'%');
+    bat3.className=Findcname(soc3);
+    bat3.innerText=soc3+'%';
+    $('#u3').text(u3);
+    $('#i3').text(i3);
 
 
     var bat8=document.getElementById("battery8");
@@ -73,19 +85,6 @@ function testBegin(val) {
 
 
 
-function batteryColor(x){
-    var color='';
-             if (x<= 30) {
-                  color='#ff0000';
-          }
-             if (x > 30) {
-                  color='#ffd800';
-          }
-             if (x > 60) {
-                  color='#00ff21';
-          }
-    return color
-}
 
 function Findcname(x){
     var cname='';
--- js/table.js
+++ js/table.js
@@ -1,23 +1,62 @@
 var i=0;
-window.onload = function () {
-    
-    updateBegin();
+var x=1;
+mydata = {
+	"name":"bms",
+	"battery": {
+        "1":{"u":"30",
+             "i":"-5",
+             "soc":"97"   
+            },
+        "2":{"u":"20",
+            "i":"-4",
+            "soc":"96"   
+           },
+        "3":{"u":"10",
+           "i":"-3",
+           "soc":"95"   
+          },
+	}
 }
 
-function updateBegin(){
-    i++;
-    console.log(i);
+window.onload = function () {
+    $("#warningled").hide();
+    updateBegin(1);
+}
 
+function updateBegin(x){
+    i++;
+    
     var d_n=new Date().getTime()+8*3600*1000;
     d=Highcharts.dateFormat('%H:%M:%S', d_n) ;
  
-    $("tbody").append('  <tr><td>'+d+'</td> <td>-5</td><td>24</td><td>97</td></tr>');
-    $("tr").eq(-2).attr("style","background-color: ");
+    $("tbody").append('  <tr><td>'+d+'</td> <td>'+mydata.battery[x]["i"]+'</td><td>'+
+    mydata.battery[x]["u"]+'</td><td>25</td><td>'+mydata.battery[x]["soc"]+'</td></tr>');
+
+    if(mydata.battery[x]["soc"]<96){
+        $("#warningled").show(100);
+
+    }
+    if(mydata.battery[x]["soc"]>=96){
+        $("#warningled").hide(100);
+
+    }
+    $("tr").eq(-2).attr("style","background-color: "); //倒数第二个变回原色
     $("tr:last").attr("style","background-color: azure");
-    t=setTimeout(updateBegin, 1000);    
+
+
+    t=setTimeout(updateBegin, 1000, x);    
     if(i==10){clearTimeout(t);}
     
 
-    
-
 }
+$(document).ready(function(){
+    $(".dropdown-item").click(function(){
+        var num=parseInt($(this).text());
+        
+        clearTimeout(t);
+        $("tbody").empty();  //只删除子元素
+        i=0;
+        updateBegin(num);
+        console.log(num);
+    });
+});
